65aaa7f90c35baf7519aa9204e81bb81dc63df03
Sudip <36448052+sudbrl@users.noreply.github.com>
2024-07-22 10:13:35 +0545

    Court.py

--- Court.py
+++ Court.py
@@ -4,14 +4,115 @@ import asyncio
 from bs4 import BeautifulSoup
 import pandas as pd
 import base64
-import warnings
-
-# Ignore warnings for simplicity
-warnings.filterwarnings("ignore")
+import io
 
 # Define the court list
 court_map = {
-    # Your court mapping here
+    'S': {
+        '264': 'सर्वोच्च अदालत'
+    },
+    'A': {
+        '4': 'उच्च अदालत जनकपुर',
+        '91': 'उच्च अदालत जनकपुर, अस्थायी इजलास वीरगन्ज',
+        '3': 'उच्च अदालत जनकपुर, राजविराज इजलास',
+        '10': 'उच्च अदालत तुलसीपुर',
+        '14': 'उच्च अदालत तुलसीपुर, नेपालगंज इजलास',
+        '9': 'उच्च अदालत तुलसीपुर, बुटवल इजलास',
+        '96': 'उच्च अदालत दिपायल',
+        '13': 'उच्च अदालत दिपायल, महेन्द्रनगर इजलास',
+        '6': 'उच्च अदालत पाटन',
+        '5': 'उच्च अदालत पाटन, हेटौंडा इजलास',
+        '7': 'उच्च अदालत पोखरा',
+        '8': 'उच्च अदालत पोखरा, बाग्लुङ्ग इजलास',
+        '1': 'उच्च अदालत विराटनगर',
+        '90': 'उच्च अदालत विराटनगर, अस्थायी इजलास ओखलढुंगा',
+        '15': 'उच्च अदालत विराटनगर, इलाम इजलास',
+        '2': 'उच्च अदालत विराटनगर, धनकुटा इजलास',
+        '11': 'उच्च अदालत सुर्खेत',
+        '12': 'उच्च अदालत सुर्खेत, जुम्ला इजलास'
+    },
+    'D': {
+        '33': 'अछाम जिल्ला अदालत',
+        '85': 'अर्धाखांची जिल्ला अदालत',
+        '43': 'ईलाम जिल्ला अदालत',
+        '53': 'उदयपुर जिल्ला अदालत',
+        '51': 'ओखलढुंगा जिल्ला अदालत',
+        '40': 'कन्चनपुर जिल्ला अदालत',
+        '89': 'कपिलवस्तु जिल्ला अदालत',
+        '70': 'काठमाण्डौं जिल्ला अदालत',
+        '68': 'काभ्रेपलान्चोक जिल्ला अदालत',
+        '29': 'कालिकोट जिल्ला अदालत',
+        '77': 'कास्की जिल्ला अदालत',
+        '35': 'कैलाली जिल्ला अदालत',
+        '52': 'खोटांग जिल्ला अदालत',
+        '84': 'गुल्मी जिल्ला अदालत',
+        '36': 'गोरखा जिल्ला अदालत',
+        '61': 'चितवन जिल्ला अदालत',
+        '24': 'जाजरकोट जिल्ला अदालत',
+        '28': 'जुम्ला जिल्ला अदालत',
+        '44': 'झापा जिल्ला अदालत',
+        '38': 'डडेलधुरा जिल्ला अदालत',
+        '34': 'डोटी जिल्ला अदालत',
+        '30': 'डोल्पा जिल्ला अदालत',
+        '75': 'तनहुं जिल्ला अदालत',
+        '45': 'ताप्लेजुंग जिल्ला अदालत',
+        '47': 'तेह्रथुम जिल्ला अदालत',
+        '18': 'दाङ जिल्ला अदालत',
+        '37': 'दार्चुला जिल्ला अदालत',
+        '23': 'दैलेख जिल्ला अदालत',
+        '66': 'दोलखा जिल्ला अदालत',
+        '49': 'धनकुटा जिल्ला अदालत',
+        '57': 'धनुषा जिल्ला अदालत',
+        '73': 'धादिंग जिल्ला अदालत',
+        '87': 'नवलपरासी जिल्ला अदालत',
+        '490': 'नवलपुर जिल्ला अदालत',
+        '72': 'नुवाकोट जिल्ला अदालत',
+        '83': 'पर्वत जिल्ला अदालत',
+        '64': 'पर्सा जिल्ला अदालत',
+        '97': 'पांचथर जिल्ला अदालत',
+        '86': 'पाल्पा जिल्ला अदालत',
+        '19': 'प्युठान जिल्ला अदालत',
+        '31': 'बझांग जिल्ला अदालत',
+        '42': 'बर्दिया जिल्ला अदालत',
+        '41': 'बांके जिल्ला अदालत',
+        '82': 'बाग्लुंग जिल्ला अदालत',
+        '32': 'बाजुरा जिल्ला अदालत',
+        '63': 'बारा जिल्ला अदालत',
+        '39': 'बैतडी जिल्ला अदालत',
+        '69': 'भक्तपुर जिल्ला अदालत',
+        '48': 'भोजपुर जिल्ला अदालत',
+        '62': 'मकवानपुर जिल्ला अदालत',
+        '80': 'मनांग जिल्ला अदालत',
+        '58': 'महोत्तरी जिल्ला अदालत',
+        '27': 'मुगु जिल्ला अदालत',
+        '79': 'मुस्तांग जिल्ला अदालत',
+        '16': 'मोरंग जिल्ला अदालत',
+        '81': 'म्याग्दी जिल्ला अदालत',
+        '74': 'रसुवा जिल्ला अदालत',
+        '65': 'रामेछाप जिल्ला अदालत',
+        '22': 'रुकुम जिल्ला अदालत',
+        '494': 'रुकुमकोट जिल्ला अदालत',
+        '88': 'रुपन्देही जिल्ला अदालत',
+        '21': 'रोल्पा जिल्ला अदालत',
+        '60': 'रौतहट जिल्ला अदालत',
+        '76': 'लमजुङ्ग जिल्ला अदालत',
+        '71': 'ललितपुर जिल्ला अदालत',
+        '46': 'संखुवासभा जिल्ला अदालत',
+        '55': 'सप्तरी जिल्ला अदालत',
+        '59': 'सर्लाही जिल्ला अदालत',
+        '20': 'सल्यान जिल्ला अदालत',
+        '67': 'सिन्धुपाल्चोक जिल्ला अदालत',
+        '56': 'सिन्धुली जिल्ला अदालत',
+        '54': 'सिराहा जिल्ला अदालत',
+        '17': 'सुनसरी जिल्ला अदालत',
+        '25': 'सुर्खेत जिल्ला अदालत',
+        '50': 'सोलुखुम्बु जिल्ला अदालत',
+        '78': 'स्यांग्जा जिल्ला अदालत',
+        '26': 'हुम्ला जिल्ला अदालत'
+    },
+    'T': {
+        '116': 'बिषेश अदालत'
+    }
 }
 
 # Mapping for display names
@@ -24,22 +125,27 @@ court_type_display_map = {
 
 # Helper function to get court names based on court type
 def get_court_names(court_type):
-    return court_map.get(court_type, {})
+    if court_type in court_map:
+        return court_map[court_type]
+    return {}
 
 # Streamlit UI
 st.title('Supreme Court Data Scraper')
 
 # Input fields for start date, end date, court type, and court ID
-start_date_str = st.text_input("Enter Start Date (YYYY-MM-DD)")
-end_date_str = st.text_input("Enter End Date (YYYY-MM-DD)")
+start_date_str = st.text_input("Enter Start Date (YYYY-MM-DD, BS)")
+end_date_str = st.text_input("Enter End Date (YYYY-MM-DD, BS)")
 
 court_type_display = st.selectbox("Select Court Type", options=list(court_type_display_map.values()))
+
+# Get the actual court type key based on display name
 court_type = [key for key, value in court_type_display_map.items() if value == court_type_display][0]
 
+# Display court names based on selected court type
 court_names = get_court_names(court_type)
 court_name_to_id = {v: k for k, v in court_names.items()}
 court_name = st.selectbox("Select Court Name", options=list(court_name_to_id.keys()))
-court_id = court_name_to_id.get(court_name, None)
+court_id = court_name_to_id[court_name] if court_name else None
 
 # Function to fetch table data from the Supreme Court website asynchronously
 async def fetch_data(session, date, court_type, court_id):
@@ -70,6 +176,7 @@ async def fetch_data(session, date, court_type, court_id):
 async def get_table_data(start_date, end_date, court_type, court_id):
     async with aiohttp.ClientSession() as session:
         tasks = []
+        # Use text-based dates directly
         date_range = pd.date_range(start=start_date, end=end_date).strftime('%Y-%m-%d').tolist()
         for faisala_date in date_range:
             tasks.append(fetch_data(session, faisala_date, court_type, court_id))
@@ -87,18 +194,20 @@ if st.button("Generate Report"):
         try:
             # Use Streamlit's asyncio feature to fetch data and show spinner
             with st.spinner("Fetching data..."):
-                loop = asyncio.new_event_loop()
-                asyncio.set_event_loop(loop)
+                loop = asyncio.get_event_loop()
                 table_data = loop.run_until_complete(get_table_data(start_date_str, end_date_str, court_type, court_id))
 
             if table_data:
                 df = pd.DataFrame(table_data)
                 df.columns = ["क्र.सं.", "दर्ता नं.", "मुद्दा नं.", "दर्ता मिति", "मुद्दाको किसिम", "मुद्दाको नाम", "वादी", "प्रतिबादी", "फैसला मिति", "पूर्ण पाठ"]
-                excel_file_path = "supreme_court_data.xlsx"
-                df.to_excel(excel_file_path, index=False)
-                with open(excel_file_path, 'rb') as f:
-                    excel_bytes = f.read()
-                b64 = base64.b64encode(excel_bytes).decode()
+                
+                # Create a BytesIO object
+                excel_buffer = io.BytesIO()
+                with pd.ExcelWriter(excel_buffer, engine='openpyxl') as writer:
+                    df.to_excel(writer, index=False)
+                
+                excel_buffer.seek(0)
+                b64 = base64.b64encode(excel_buffer.read()).decode()
                 href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="court_data.xlsx">Download Excel file</a>'
                 st.markdown(href, unsafe_allow_html=True)
                 
